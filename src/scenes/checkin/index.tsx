ee2ca58ec95fee59195762dafb5d391702e7b123
toledompm <toledompm@gmail.com>
2021-11-16 21:02:19 -0300

    refactor

--- src/scenes/checkin/index.tsx
+++ src/scenes/checkin/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
 import { Camera, BarCodeScanningResult } from 'expo-camera';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { colors, mainSceneStyle } from 'styles';
@@ -9,9 +9,7 @@ function checkin(url: string, userToken: string): JSX.Element {
   const [hasPermission, setHasPermission] = React.useState(false);
 
   const getPermissions = async () => {
-    console.log('getPermissions');
     const { status } = await Camera.requestCameraPermissionsAsync();
-    console.log('getPermissions', status);
     setHasPermission(status === 'granted');
   };
 
@@ -26,7 +24,6 @@ function checkin(url: string, userToken: string): JSX.Element {
   };
 
   if (!hasPermission) {
-    alert('No camera permissions!');
     getPermissions();
     return (
       <SafeAreaView style={mainSceneStyle.container}>
@@ -37,7 +34,7 @@ function checkin(url: string, userToken: string): JSX.Element {
             color={colors.contrast}
             style={{ marginTop: 100 }}
           />
-          <Icon name="camera" size={200} color={colors.contrast} style={{ marginTop: 180 }} />
+          <Text> No Camera Permissions! </Text>
         </View>
       </SafeAreaView>
     );
@@ -50,14 +47,14 @@ function checkin(url: string, userToken: string): JSX.Element {
           name={'qrcode'}
           size={150}
           color={colors.contrast}
-          style={{ marginTop: 180 }}
+          style={{ marginTop: 100 }}
         />
       </View>
       <View style={{ flex: 1, alignSelf: 'stretch' }}>
         <Camera
           style={{
-            flex: 1,
-            width: '100%',
+            width: '50%',
+            height: '50%',
           }}
           onBarCodeScanned={(scan) => checkinButtonAction(scan)}
         ></Camera>
